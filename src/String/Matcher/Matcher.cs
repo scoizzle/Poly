3f2501a4d69f52f6e921adf00847b9eda4f9cf49
Scot Murphy <scot@scoizzle.com>
2018-08-25 07:55:47 -0500

    Enabled Minimum string length optimization for String.Matcher

--- src/String/Matcher/Matcher.cs
+++ src/String/Matcher/Matcher.cs
@@ -12,38 +12,61 @@ namespace Poly.String {
         protected RawTemplateDelegate raw_templater;
 
         public readonly string Format;
+        public readonly int MinimumLength;
         public readonly TypeInformation TypeInfo;
 
         public Matcher(string format) {
             Format = format;
             TypeInfo = TypeInformation.Get<T>();
 
-            Parse(format, out comparer, out extracter, out raw_extracter, out templater, out raw_templater);
+            Parse(format, out comparer, out extracter, out raw_extracter, out templater, out raw_templater, out MinimumLength);
         }
 
         public bool Compare(string text) => 
-            comparer(text);
+            text?.Length > MinimumLength ?
+                comparer(text) :
+                false;
 
         public bool Compare(StringIterator it) => 
-            comparer(it);
+            it?.Length > MinimumLength ?
+                comparer(it) :
+                false;
 
         public bool Extract(string text, T obj) => 
-            extracter(text, obj);
+            text?.Length > MinimumLength ?
+                extracter(text, obj) :
+                false;
 
         public bool Extract(StringIterator it, T obj) => 
-            extracter(it, obj);
+            it?.Length > MinimumLength ?
+                extracter(it, obj) :
+                false;
 
-        public bool Extract(string text, out T obj) =>
-            extracter(text, obj = TypeInfo.CreateInstance<T>());
+        public bool Extract(string text, out T obj) {
+            if (text?.Length > MinimumLength) 
+                return extracter(text, obj = TypeInfo.CreateInstance<T>());
 
-        public bool Extract(StringIterator it, out T obj) =>
-            extracter(it, obj = TypeInfo.CreateInstance<T>());
+            obj = default;
+            return false;
+        }
+
+        public bool Extract(StringIterator it, out T obj) {
+            if (it?.Length > MinimumLength) 
+                return extracter(it, obj = TypeInfo.CreateInstance<T>());
+
+            obj = default;
+            return false;
+        }
 
         public bool Extract(string text, TrySetMemberDelegate set) =>
-            raw_extracter(text, set);
+            text?.Length > MinimumLength ?
+                raw_extracter(text, set) :
+                false;
 
         public bool Extract(StringIterator it, TrySetMemberDelegate set) =>
-            raw_extracter(it, set);
+            it?.Length > MinimumLength ?
+                raw_extracter(it, set) :
+                false;
 
         public bool Template(StringBuilder output, T obj) =>
             templater(output, obj);
@@ -69,7 +92,8 @@ namespace Poly.String {
             out ExtractDelegate extract, 
             out RawExtractDelegate raw_extract, 
             out TemplateDelegate template,
-            out RawTemplateDelegate raw_template
+            out RawTemplateDelegate raw_template,
+            out int min_length
         ) {
             var context = new Context(TypeInfo);
 
@@ -81,6 +105,7 @@ namespace Poly.String {
                 raw_extract = delegates.RawExtract;
                 template = delegates.Template;
                 raw_template = delegates.RawTemplate;
+                min_length = context.MinimumLength;
                 return true;
             }
 
@@ -89,6 +114,7 @@ namespace Poly.String {
             raw_extract = default;
             template = default;
             raw_template = default;
+            min_length = int.MaxValue;
             return false;
         }
     }

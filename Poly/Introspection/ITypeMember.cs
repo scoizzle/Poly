38d39fb8d37d26c8884b8ab19d430c2bbb50b03f
Scot Murphy <scot@scoizzle.com>
2025-11-29 19:23:47 -0600

    refactor(ITypeMember): Move Name property declaration to the top for improved readability

--- Poly/Introspection/ITypeMember.cs
+++ Poly/Introspection/ITypeMember.cs
@@ -3,9 +3,9 @@ using Poly.Interpretation;
 namespace Poly.Introspection;
 
 public interface ITypeMember {
+    string Name { get; }
     ITypeDefinition MemberTypeDefinition { get; }
     ITypeDefinition DeclaringTypeDefinition { get; }
-    string Name { get; }
     IEnumerable<IParameter>? Parameters { get; }
 
     Value GetMemberAccessor(Value instance, params IEnumerable<Value>? parameters);

76382a4779462bedfb54fd123a9bfbac9efb929c
Scot Murphy <scot@scoizzle.com>
2026-01-16 22:00:18 -0600

    feat: Add DataModelingContext and Validator classes for data validation

---
+++ Poly/DataModeling/DataModelingContext.cs
@@ -0,0 +1,11 @@
+
+namespace Poly.DataModeling;
+
+public sealed class DataModelingContext {
+    private readonly DataModelTypeDefinitionProvider _typeDefinitionProvider;
+
+    public DataModelingContext()
+    {
+        _typeDefinitionProvider = new DataModelTypeDefinitionProvider();
+    }
+}

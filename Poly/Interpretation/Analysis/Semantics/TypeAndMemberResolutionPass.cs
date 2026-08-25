1f084fa974a59481aa527a08fd086631b11fd76f
Scot Murphy <scot@scoizzle.com>
2026-08-24 22:30:09 -0500

    feat(semantics): CallExternal resolves to void in TypeAndMemberResolutionPass

--- Poly/Interpretation/Analysis/Semantics/TypeAndMemberResolutionPass.cs
+++ Poly/Interpretation/Analysis/Semantics/TypeAndMemberResolutionPass.cs
@@ -72,6 +72,7 @@ internal sealed class TypeAndMemberResolver : INodeAnalyzer {
                 ? ResolveNodeType(context, swt.Cases[0].Body)
                 : swt.DefaultCase is not null ? ResolveNodeType(context, swt.DefaultCase) : null,
             Await aw => ResolveNodeType(context, aw.Operand),
+            CallExternal => context.TypeDefinitions.GetTypeDefinition(typeof(void)),
             _ => null
         };
     }
